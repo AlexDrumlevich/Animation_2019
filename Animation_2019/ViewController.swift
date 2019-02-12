1f34d57c8e37345076141ea6a89fca31aa170de1
Друмлевич <drumlevic@macbook-pro-drumlevic.local>
2019-02-12 19:17:58 +0500

    Initial Commit

---
+++ Animation_2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation_2019
+//
+//  Created by Друмлевич on 12.02.2019.
+//  Copyright © 2019 Alexey Drumlevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
